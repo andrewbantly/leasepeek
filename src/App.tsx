094b0bc4a63aa0c24dce16d0bce406f5ad535c08
andrewbantly <andrewbantly@gmail.com>
2023-10-02 19:55:37 -0700

    pinging backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,53 @@ import * as React from "react"
 import './assets/index.css'
 import {
   BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
+import { ChakraProvider, theme } from "@chakra-ui/react"
 import NavBar from "./components/partials/navBar"
 import { Login } from "./pages/login/login"
 import { Home } from "./pages/home/home"
 import { SignUp } from "./pages/signup/signup"
 import { NotFound } from "./pages/notfound/notfound"
+import { useState, useEffect } from 'react'
+import jwt_decode from 'jwt-decode'
+import axios from "axios"
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Router>
-      <NavBar></NavBar>
-      <Routes>
-        <Route
-          Component={Home}
-          path='/'
-        ></Route>
-        <Route
-          Component={Login}
-          path='/login'
-        ></Route>
-        <Route
-          Component={SignUp}
-          path='/register'
-        ></Route>     
-        <Route
-          Component={NotFound}
-          path="*"
-        ></Route>   
-      </Routes>
-    </Router>
-  </ChakraProvider>
-)
+
+export const App = () => {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt')
+    if (token) {
+      setCurrentUser(jwt_decode(token))
+    } else {
+      setCurrentUser(null)
+    }
+  }, [])
+  
+  return ( 
+    <ChakraProvider theme={theme}>
+      <Router>
+        <NavBar></NavBar>
+        <Routes>
+          <Route
+            element={<Home/>}
+            path='/'
+          ></Route>
+          <Route
+            element={<Login/>}
+            path='/login'
+          ></Route>
+          <Route
+            element={<SignUp/>}
+            path='/register'
+          ></Route>
+          <Route
+            element={<NotFound/>}
+            path="*"
+          ></Route>
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  )
+
+}
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,61 @@
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button,
+} from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {User} from '../../data/tempUser'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+
 export function Login() {
-    return(
-        <p>login</p>
+
+    const [email, setEmail] = useState(User.email)
+    const [password, setPassword] = useState(User.password); 
+
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log("request sent")
+        try {
+            const request = {
+                email,
+                password
+            };
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/user/login`, request);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input type='email' value={User.email} onChange={handleEmailChange} />
+            <FormHelperText>
+                We will never share your email.
+            </FormHelperText>
+            <FormLabel>Password</FormLabel>
+            <Input type='password' value={User.password} />
+        </FormControl>
+        <Button
+            mt={4}
+            loadingText='Logging you in'
+            colorScheme='teal'
+            type='submit'
+          >
+            Submit
+          </Button>
+    </form>
     )
 }
