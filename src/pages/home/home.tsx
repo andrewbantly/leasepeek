01f3ffdef6e13d48686a8a8f89ff5dc88d041785
andrewbantly <andrewbantly@gmail.com>
2023-10-02 18:49:20 -0700

    apex charts init

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,11 @@
+import { rentRollData } from '../../data/test_data'
+import ApexChart from 'react-apexcharts';
+
+
 export function Home() {
-    return(
-        <p>home</p>
-    )
+    return (
+        <>
+            <h2>User home page</h2>
+        </>
+    );
 }
