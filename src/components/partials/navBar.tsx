8edab69a8359eefb8f31e82b8d9f0d18b5af1702
andrewbantly <andrewbantly@gmail.com>
2023-10-04 18:45:53 -0700

    made LeasePeek navigate home

--- src/components/partials/navBar.tsx
+++ src/components/partials/navBar.tsx
@@ -3,11 +3,13 @@ import { Link as ChakraLink } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ColorModeSwitcher } from "../ui/ColorModeSwitcher";
 import { NavBarProps } from "../../interfaces/navBarProps";
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar({ currentUser, setCurrentUser, handleLogout }: NavBarProps) {
     const bg = useColorModeValue("gray.200", "gray.700");
     const color = useColorModeValue("gray.800", "white");
     const hoverColor = useColorModeValue("blue.800", "blue.400");
+    const navigate = useNavigate()
 
     const loginLogoutLink = currentUser 
       ? <Button as={Link} to="/" onClick={handleLogout} variant="link" _hover={{ color: hoverColor, textDecoration: "none" }}>Logout</Button>
@@ -16,7 +18,7 @@ export default function NavBar({ currentUser, setCurrentUser, handleLogout }: Na
     return (
         <Box bg={bg} color={color} px={4} py={2} boxShadow="md">
             <Flex>
-                <Heading size="lg">LeasePeek</Heading>
+                <Heading size="lg" cursor="pointer" onClick={() => navigate('/')}>LeasePeek</Heading>
                 <Spacer />
                 <Flex align="center">
                     <ChakraLink as={Link} to='/' mr={4} fontWeight="medium" _hover={{ color: hoverColor, textDecoration: "none" }}>Home</ChakraLink>
