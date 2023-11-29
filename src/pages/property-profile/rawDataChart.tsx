29647e6fe198af2d780cc2b5baa427fda4a68d3c
andrewbantly <andrewbantly@gmail.com>
2023-11-28 19:45:39 -0800

    data starting to appear in grid

--- src/pages/property-profile/rawDataChart.tsx
+++ src/pages/property-profile/rawDataChart.tsx
@@ -1,10 +1,63 @@
 import { PropertyResponseObject } from "../../interfaces/propertyProfile/propertyProfileProps";
-import { ApexGrid } from 'apex-grid';
+
+import * as React from 'react';
+import { createComponent } from '@lit-labs/react';
+import type { EventName } from '@lit-labs/react';
+import {
+  ApexFilteringEvent,
+  ApexFilteredEvent,
+  ApexGrid,
+  ColumnConfiguration,
+  FilterExpression,
+  SortExpression,
+} from 'apex-grid';
+
+ApexGrid.register();
+
+function createApexGridWrapper<T extends object>() {
+  return createComponent({
+    tagName: 'apex-grid',
+    elementClass: ApexGrid<T>,
+    react: React,
+    events: {
+      onSorting: 'sorting' as EventName<CustomEvent<SortExpression<T>>>,
+      onSorted: 'sorted' as EventName<CustomEvent<SortExpression<T>>>,
+      onFiltering: 'filtering' as EventName<CustomEvent<ApexFilteringEvent<T>>>,
+      onFiltered: 'filtered' as EventName<CustomEvent<ApexFilteredEvent<T>>>,
+    },
+  });
+}
+
+interface PropertyDataItem {
+    market: number;
+    rent: number;
+    sqft: number;
+    status: string | null;
+    unit: string;
+}
 
 export function RawBuildingDataComponent(propertyDataObject: PropertyResponseObject) {
-    return(
+
+    const columns: ColumnConfiguration<PropertyDataItem>[] = [
+        { key: 'unit', headerText: "Unit"},
+        { key: 'status', headerText: "Status"},
+        { key: 'sqft', headerText: "SqFt" },
+        { key: 'rent', headerText: "Rent" },
+        { key: 'market', headerText: "Market" },
+    ];
+
+    const data = propertyDataObject.data;
+
+    const PropertyGrid = createApexGridWrapper<PropertyDataItem>();
+
+    return (
         <>
-        Raw Building Data
+            <h2>Raw Building Data</h2>
+            <PropertyGrid 
+                columns={columns} 
+                data={data}
+                className="custom-grid-style"
+            />
         </>
     )
 }
