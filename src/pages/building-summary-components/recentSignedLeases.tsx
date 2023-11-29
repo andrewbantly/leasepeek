59de33b6e2866efdf9503f935af4233b71213d0c
andrewbantly <andrewbantly@gmail.com>
2023-11-28 18:56:49 -0800

    recent signed lease chart refactored to integrate lease signed annotations

--- src/pages/building-summary-components/recentSignedLeases.tsx
+++ src/pages/building-summary-components/recentSignedLeases.tsx
@@ -10,7 +10,6 @@ interface RecentLeaseProps {
 
 export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProps) {
     const textColor = useColorModeValue("gray.800", 'white');
-    const additionalInformationColor = useColorModeValue("#1A202C", "#A0AEC0");
 
     let sqFt180Days: number = 0;
     let avgRent180Days: number = 0;
@@ -35,13 +34,13 @@ export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProp
     let sqFt30Days: number = 0;
     let avgRent30Days: number = 0;
     let leaseCount30Days: number = 0;
-    
+
     let sqFtDataExists = false
 
     for (const key in recentLeases) {
         const rentData = recentLeases[key];
         const sqFtData = floorplans[key].avgSqft
-        
+
         if (sqFtData > 0) {
             sqFtDataExists = true
         }
@@ -64,19 +63,19 @@ export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProp
         if (rentData.recent_leases.last_120_days.average_rent) {
             sqFt120Days += sqFtData
         }
-        
+
         leaseCount90Days += rentData.recent_leases.last_90_days.count
         avgRent90Days += rentData.recent_leases.last_90_days.average_rent
         if (rentData.recent_leases.last_90_days.average_rent > 0) {
             sqFt90Days += sqFtData
         }
-        
+
         leaseCount60Days += rentData.recent_leases.last_60_days.count
         avgRent60Days += rentData.recent_leases.last_60_days.average_rent
         if (rentData.recent_leases.last_60_days.average_rent) {
             sqFt60Days += sqFtData
         }
-        
+
         leaseCount30Days += rentData.recent_leases.last_30_days.count
         avgRent30Days += rentData.recent_leases.last_30_days.average_rent
         if (rentData.recent_leases.last_30_days.average_rent) {
@@ -94,7 +93,7 @@ export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProp
     const labels: string[] = ['Previous 180 Days', 'Previous 150 Days', 'Previous 120 Days', 'Previous 90 Days', 'Previous 60 Days', 'Previous 30 Days']
     const series = [{
         name: "Avg Lease per SqFt ($)",
-        data: [rent180DaysAvg,rent150DaysAvg, rent120DaysAvg, rent90DaysAvg, rent60DaysAvg, rent30DaysAvg]
+        data: [rent180DaysAvg, rent150DaysAvg, rent120DaysAvg, rent90DaysAvg, rent60DaysAvg, rent30DaysAvg]
     }];
 
     const options = {
@@ -115,10 +114,19 @@ export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProp
         },
         xaxis: {
             categories: labels,
+                        max: function (maxValue: number) {
+                return maxValue * 1.01;
+            }
         },
         yaxis: {
             title: {
                 text: 'Avg Lease per SqFt ($)'
+            },
+            max: function (maxValue: number) {
+                return maxValue * 1.05;
+            },
+            min: function (minValue: number) {
+                return minValue * 0.95;
             }
         },
         title: {
@@ -127,54 +135,99 @@ export function RecentSignedLeases({ recentLeases, floorplans }: RecentLeaseProp
         },
         tooltip: {
             theme: useColorModeValue('light', 'dark')
+        },
+        annotations: {
+            xaxis: [
+                {
+                    x: 'Previous 180 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount180Days}`,
+                    }
+                },
+                {
+                    x: 'Previous 150 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount150Days}`,
+                    }
+                },
+                {
+                    x: 'Previous 120 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount120Days}`,
+                    }
+                },
+                {
+                    x: 'Previous 90 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount90Days}`,
+                    }
+                },
+                {
+                    x: 'Previous 60 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount60Days}`,
+                    }
+                },
+                {
+                    x: 'Previous 30 Days',
+                    borderWidth: 0,
+                    label: {
+                        style: {
+                            color: 'white',
+                            background: '#2F855A',
+                            fontWeight: 'bold',
+                        },
+                        orientation: 'horizontal',
+                        text: `Leases signed: ${leaseCount30Days}`,
+                    }
+                },
+            ]
         }
     } as any;
 
-    const leaseCount180DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 180 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount180Days}</Text></Text>
-    </>
-
-    const leaseCount150DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 150 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount150Days}</Text></Text>
-    </>
-
-    const leaseCount120DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 120 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount120Days}</Text></Text>
-    </>
-
-    const leaseCount90DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 90 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount90Days}</Text></Text>
-    </>
-
-    const leaseCount60DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 60 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount60Days}</Text></Text>
-    </>
-
-    const leaseCount30DayNote = 
-    <>
-    <Text color={additionalInformationColor} size="sm">Leases signed in the last 30 days:<Text display='inline' ml={1} fontSize="md" color="teal.500" fontWeight='bold'>{leaseCount30Days}</Text></Text>
-    </>
-
     const chart = <Chart options={options} series={series} type="line" height="300px" />
 
-    const noSqFtDataResponse = <Text fontWeight="bold" fontSize='md' color={textColor} mb={5}><Text  fontWeight="bold" fontSize='md' color='red' display='inline'>Error: </Text>Cannot create chart. SqFt data not included in rent roll.</Text>
-
-
+    const noSqFtDataResponse = <Text fontWeight="bold" fontSize='md' color={textColor} mb={5}><Text fontWeight="bold" fontSize='md' color='red' display='inline'>Error: </Text>Cannot create chart. SqFt data not included in rent roll.</Text>
 
     return (
         <>
-        {sqFtDataExists ? chart : noSqFtDataResponse}
-        {leaseCount180Days > 0 ? leaseCount180DayNote : ''}
-        {leaseCount150Days > 0 ? leaseCount150DayNote : ''}
-        {leaseCount120Days > 0 ? leaseCount120DayNote : ''}
-        {leaseCount90Days > 0 ? leaseCount90DayNote : ''}
-        {leaseCount60Days > 0 ? leaseCount60DayNote : ''}
-        {leaseCount30Days > 0 ? leaseCount30DayNote : ''}
+            {sqFtDataExists ? chart : noSqFtDataResponse}
         </>
     )
 }
