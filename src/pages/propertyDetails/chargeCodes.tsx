239146a3d1ad7fe673e98bdfa9df953165718c25
andrewbantly <andrewbantly@gmail.com>
2023-12-06 11:31:33 -0800

    unit status forms

--- src/pages/propertyDetails/chargeCodes.tsx
+++ src/pages/propertyDetails/chargeCodes.tsx
@@ -1,9 +1,51 @@
+import {
+    Box,
+    Text,
+    Select,
+    Input,
+    useColorModeValue,
+    FormLabel,
+    InputGroup,
+    Grid,
+    Button,
+} from '@chakra-ui/react';
 import { PropertyResponseObject } from "../../interfaces/propertyProfile/propertyProfileProps";
+import { ChargeCodesForm } from './forms/chargeCodesForm';
 
 interface ChargeCodesProps {
     propertyDataObject: PropertyResponseObject;
 }
 
 export function ChargeCodes({ propertyDataObject }: ChargeCodesProps) {
-    return (<>Charge codes</>)
+    const floorPlanTableBgColor = useColorModeValue("white", "gray.700");
+    const charges: { [key: string]: number } = {};
+
+    propertyDataObject.data.forEach(unit => {
+        unit.charges.forEach(charge => {
+            if (charges[charge.code]) {
+                charges[charge.code] += charge.value;
+            } else {
+                charges[charge.code] = charge.value;
+            }
+        });
+    });
+
+    return (
+        <Box p={6} borderRadius="lg" borderWidth="1px" boxShadow="xl" bg={floorPlanTableBgColor} display="flex" flexDirection="column" mb={4}>
+            <Text fontSize='xl' fontWeight='bold' mb={2}>Additional Charge Codes
+            </Text>
+            <Grid
+                templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+                gap={4}
+            >
+                {Object.entries(charges).map(([charge, value]) => (
+                    <ChargeCodesForm
+                        key={charge}
+                        charge={charge}
+                        value={value}
+                    />
+                ))}
+            </Grid>
+        </Box>
+    )
 }
---
+++ src/pages/propertyDetails/forms/chargeCodesForm.tsx
@@ -0,0 +1,54 @@
+import {
+    Box,
+    Text,
+    Select,
+    Input,
+    useColorModeValue,
+    FormLabel,
+    InputGroup,
+    Grid,
+    Button,
+} from '@chakra-ui/react';
+
+interface ChargeCodesFormProps {
+    charge: string;
+    value: number;
+}
+
+export function ChargeCodesForm({ charge, value }: ChargeCodesFormProps) {
+    console.log(`charge: ${charge} value: ${value}`)
+
+    function formatCurrency(amount: number): string {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        }).format(amount);
+    };
+
+
+    return (
+        <InputGroup>
+            <FormLabel width={'100px'}>
+                Charge: {charge}
+                <Text size={'sm'} color={'gray.500'}>
+                    {formatCurrency(value)}
+                </Text>
+            </FormLabel>
+            <Select
+                size="sm"
+                width="200px"
+                defaultValue={''}
+            >
+                <option value="" disabled hidden>Select an option</option>
+                <option>Contractual Rent</option>
+                <option>Supplemental Rent</option>
+                <option>Recurring Concessions</option>
+                <option>Upfront Concessions</option>
+                <option>Empl./Other discount</option>
+                <option>Other Income</option>
+            </Select>
+        </InputGroup>
+    )
+}
