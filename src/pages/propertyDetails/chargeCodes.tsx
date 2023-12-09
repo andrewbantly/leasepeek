81bfa83ef8a8968fc14ba2e71c38c391d2ecae7b
andrewbantly <andrewbantly@gmail.com>
2023-12-09 11:16:41 -0800

    refactor how charges are loaded

--- src/pages/propertyDetails/chargeCodes.tsx
+++ src/pages/propertyDetails/chargeCodes.tsx
@@ -1,24 +1,42 @@
+import { useState, useEffect, FormEvent } from 'react';
+import { useParams } from 'react-router-dom';
 import { Box, Text, useColorModeValue, Grid, } from '@chakra-ui/react';
 import { PropertyResponseObject } from "../../interfaces/propertyProfile/propertyProfileProps";
 import { ChargeCodesForm } from './forms/chargeCodesForm';
+import axios from 'axios';
 
 interface ChargeCodesProps {
     propertyDataObject: PropertyResponseObject;
 }
 
-export function ChargeCodes({ propertyDataObject }: ChargeCodesProps) {
-    const floorPlanTableBgColor = useColorModeValue("white", "gray.700");
-    const charges: { [key: string]: number } = {};
+type ChargeCodes = { [key: string]: number };
 
-    propertyDataObject.data.forEach(unit => {
-        unit.charges.forEach(charge => {
-            if (charges[charge.code]) {
-                charges[charge.code] += charge.value;
-            } else {
-                charges[charge.code] = charge.value;
-            }
+export function ChargeCodes({ propertyDataObject }: ChargeCodesProps) {
+
+    const { objectId } = useParams();
+
+    const [charges, setCharges] = useState<{ [key: string]: number }>({});
+    const [chargeCodesState, setChargesState] = useState<{ [key: string]: number }>({});
+
+    useEffect(() => {
+        const newCharges: ChargeCodes = {};
+
+        propertyDataObject.data.forEach(unit => {
+            unit.charges.forEach(charge => {
+                if (newCharges[charge.code]) {
+                    newCharges[charge.code] += charge.value;
+                } else {
+                    newCharges[charge.code] = charge.value;
+                }
+            });
         });
-    });
+
+        setCharges(newCharges);
+        setChargesState(newCharges);
+    }, [propertyDataObject.data]);
+
+                    
+    const floorPlanTableBgColor = useColorModeValue("white", "gray.700");
 
     return (
         <Box p={6} borderRadius="lg" borderWidth="1px" boxShadow="xl" bg={floorPlanTableBgColor} display="flex" flexDirection="column" mb={4}>
